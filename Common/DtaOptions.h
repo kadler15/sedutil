8dc46634649ce2fabe80d6cd26965597b6f8385e
Kris Adler <spurfan15@gmail.com>
2024-11-06 18:17:49 -0600

    Add commands for controlling the Makers Authority (#2)

--- Common/DtaOptions.h
+++ Common/DtaOptions.h
@@ -104,6 +104,9 @@ typedef enum _sedutiloption {
     prepareForS3Sleep,
 	rawCmd,
     addUserToLockingACEs,
+	disableMakersAuthority,
+	enableMakersAuthority,
+	printMakersAuthorityStatus,
 } sedutiloption;
 
 /** verify the number of arguments passed */
